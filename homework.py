9d8dd6c04f1fcd3908b434a9e79e61d1129887cf
korol787 <63867207+korol787@users.noreply.github.com>
2020-07-02 15:17:55 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,29 +1,26 @@
 import datetime as dt
 
 today_date = dt.date.today()
-USD_RATE = 68
-EURO_RATE = 75
 
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
     def add_record(self, record):
-        self.records.append(record.list1)
+        self.records.append(record)
     def get_today_stats(self):
         amount_today = 0
-        for i in range(len(self.records)):
-            if self.records[i][2] == today_date:
-                amount_today += self.records[i][0]
+        for record in self.records:
+            if record.date == today_date:
+                amount_today += record.amount
         return amount_today
     def get_week_stats(self):
         amount_week = 0
         week_ago = today_date - dt.timedelta(days=6)
-        for i in range(len(self.records)):
-            if week_ago <= self.records[i][2] <= today_date:
-                amount_week += self.records[i][0]
+        for record in self.records:
+            if week_ago <= record.date <= today_date:
+                amount_week += record.amount
         return amount_week
-    
 class Record:
     def __init__(self, amount, comment, date=None):
         self.amount = amount
@@ -34,31 +31,26 @@ class Record:
             date_format = '%d.%m.%Y'
             moment = dt.datetime.strptime(date, date_format)
             self.date = moment.date()
-        self.list1 = []
-        self.list1.append(self.amount)
-        self.list1.append(self.comment)
-        self.list1.append(self.date)
-
-def coefficient_and_currency(currency):
-    if currency == 'rub':
-        coef = 1
-        currency = 'руб'
-    elif currency == 'usd':
-        coef = USD_RATE
-    else:
-        coef = EURO_RATE
-        currency = 'Euro'
-    return coef, currency
 
 class CashCalculator(Calculator):
+    EURO_RATE = 75.01
+    USD_RATE = 68.01
+    def __init__(self, limit):
+        super().__init__(limit)
     def get_today_cash_remained(self, currency):
-        coef, currency = coefficient_and_currency(currency)
-        if currency == 'usd':
+        if currency == 'rub':
+            coef = 1
+            currency = 'руб'
+        elif currency == 'usd':
+            coef = self.USD_RATE
             currency = currency.upper()
+        else:
+            coef = self.EURO_RATE
+            currency = 'Euro'
         cash_today = 0
-        for i in range(len(self.records)):
-            if self.records[i][2] == today_date:
-                cash_today += self.records[i][0]
+        for record in self.records:
+            if record.date == today_date:
+                cash_today += record.amount
         if cash_today < self.limit:   
             cash_balance = (self.limit - cash_today)/coef
             return 'На сегодня осталось {:.2f} {}'.format(cash_balance, currency)
@@ -71,12 +63,11 @@ class CashCalculator(Calculator):
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         calories_today = 0
-        for i in range(len(self.records)):
-            if self.records[i][2] == today_date:
-                calories_today += self.records[i][0]
+        for record in self.records:
+            if record.date == today_date:
+                calories_today += record.amount
         if calories_today < self.limit:
             calories_for_today = self.limit - calories_today
             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories_for_today} кКал'
         else:
             return 'Хватит есть!'
-
